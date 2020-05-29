a35e312b8cb0c7b89a7095f4da1bed3066eca412
fuzzylogic2000 <katharina.albers@liqd.de>
2020-05-29 12:31:10 +0200

    add resistance model

--- liqd_lunch/consent/admin.py
+++ liqd_lunch/consent/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Restaurant)
+admin.site.register(models.Resistance)
--- liqd_lunch/consent/models.py
+++ liqd_lunch/consent/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
 
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-    link = models.URLField(blank=True)
+    name = models.CharField(
+        max_length=255
+    )
+    link = models.URLField(
+        blank=True
+    )
 
     def __str__(self):
         return self.name
+
+
+class Resistance(models.Model):
+    resistance = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MaxValueValidator(10)]
+    )
+    restaurant = models.ForeignKey(
+        'Restaurant',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(
+        editable=False,
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return '{}_{}'.format(self.pk, self.restaurant.name)
