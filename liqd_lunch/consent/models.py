64dc06b292d1cbcb6efe93c1070f10f77800de65
fuzzylogic2000 <katharina.albers@liqd.de>
2020-05-29 12:06:17 +0200

    add restaurant model

--- liqd_lunch/consent/models.py
+++ liqd_lunch/consent/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
