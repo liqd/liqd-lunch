77f420612cd0e2c8a0d99ca5ecad870641b3c4c4
Rineee <k.schmid@liqd.net>
2020-06-03 17:56:29 +0200

    consent/views: remove resistance detail

--- liqd_lunch/consent/views.py
+++ liqd_lunch/consent/views.py
@@ -105,27 +105,3 @@ def resistance_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def resistance_detail(request, pk):
-
-    try:
-        resistance = models.Resistance.objects.get(pk=pk)
-    except models.Resistance.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = serializers.ResistanceSerializer(resistance,context={'request': request})
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = serializers.ResistanceSerializer(resistance, data=request.data,context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        resistance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
