f59c21d1f96adaf9086c74283d3ee48e40b424c1
Rineee <k.schmid@liqd.net>
2020-06-03 17:46:52 +0200

    frontend: remove ResistanceCreateUpdate

--- liqd_lunch/frontend/src/App.js
+++ liqd_lunch/frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 
 import  ResistanceList from './ResistanceList'
-import  ResistanceCreateUpdate  from './ResistanceCreateUpdate'
 import  RestaurantsList from './RestaurantsList'
 import  RestaurantCreateUpdate  from './RestaurantCreateUpdate'
 import './App.css';
@@ -16,7 +15,6 @@ const BaseLayout = () => (
         <a className="nav-item nav-link" href="/">Restaurants</a>
         <a className="nav-item nav-link" href="/restaurant">Create Restaurant</a>
         <a className="nav-item nav-link" href="/resistances">Resistance List</a>
-        <a className="nav-item nav-link" href="/resistance">Create Resistance</a>
       </div>
     </nav>
 
@@ -25,8 +23,6 @@ const BaseLayout = () => (
       <Route path="/restaurant/:pk"  component={RestaurantCreateUpdate} />
       <Route path="/restaurant/" exact component={RestaurantCreateUpdate} />
       <Route path="/resistances/" exact component={ResistanceList} />
-      <Route path="/resistance/:pk"  component={ResistanceCreateUpdate} />
-      <Route path="/resistance/" exact component={ResistanceCreateUpdate} />
     </div>
 
   </div>
