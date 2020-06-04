d88030e270056cd1f34c6508afc15d4a33253375
Phillippa <p.morland@liqd.net>
2020-06-04 16:57:01 +0200

    RestaurantList: add state object to collect resistance scores to send together on submit

--- liqd_lunch/frontend/src/RestaurantsList.js
+++ liqd_lunch/frontend/src/RestaurantsList.js
@@ -11,11 +11,12 @@ class  RestaurantsList  extends  Component {
         super(props);
         this.state  = {
             restaurants: [],
-            nextPageURL:  ''
+            nextPageURL:  '',
+            resistanceScore: {}
         };
         this.nextPage  =  this.nextPage.bind(this);
         this.handleDelete  =  this.handleDelete.bind(this);
-        this.handleResistanceCreate  =  this.handleResistanceCreate.bind(this);
+        this.handleFormSubmit  =  this.handleFormSubmit.bind(this);
         this.resistanceRef = React.createRef()
     }
 
@@ -29,7 +30,7 @@ class  RestaurantsList  extends  Component {
         });
     }
 
-    handleDelete(e,pk){
+    handleDelete(e, pk){
         var  self  =  this;
         restaurantsService.deleteRestaurant({pk :  pk}).then(()=>{
             var  newArr  =  self.state.restaurants.filter(function(obj) {
@@ -40,18 +41,31 @@ class  RestaurantsList  extends  Component {
         });
     }
 
-    handleResistanceCreate(e,rest_pk){
-        //var  self  =  this;
-        resistanceService.createResistance(
-          {
+    handleSliderChange(e, rest_pk) {
+      var newResistanceScore = this.state.resistanceScore;
+      newResistanceScore[rest_pk] = e.target.value;
+
+      this.setState({
+        resistanceScore:  newResistanceScore
+      })
+    }
+
+    handleFormSubmit(){
+      const promiseList = []
+      for (const [rest_pk, resistance] of Object.entries(this.state.resistanceScore)) {
+        let promise = resistanceService.createResistance({
             "restaurant": rest_pk,
-            "resistance": e.target.value
-        }
-        ).then((result)=>{
-          '';
+            "resistance": resistance
         }).catch(()=>{
           alert('There was an error! Please re-check your form.');
         });
+        promiseList.push(promise)
+      }
+      Promise.all(promiseList).then((values) => {
+        alert('Yey! Your answers have been submitted.');
+      }).catch(()=>{
+        alert('Liquid Lunch is currently offline, please try again when online.');
+      });
     }
 
     nextPage(){
@@ -67,22 +81,20 @@ class  RestaurantsList  extends  Component {
         return (
             <div className="restaurants--list row justify-content-center">
               <div className="col-sm-8 col-md-6 col-lg-4">
-                <form onSubmit={this.handleResistanceCreate}>
                 {this.state.restaurants.map( c  =>
                     <div className="py-3 form-group" key={c.pk}>
                         <div className="d-flex justify-content-between">
                             <label htmlFor={"restaurant" + c.pk}>{c.name}</label>
-                            <button className="btn btn-link" onClick={(e)=>  this.handleDelete(e,c.pk) }> Delete</button>
+                            <button className="btn btn-link" onClick={(e)=>  this.handleDelete(e, c.pk) }> Delete</button>
                             <a className="btn btn-link" href={"/restaurant/" + c.pk}> Update</a>
                         </div>
-                        <input type="range" ref={this.resistanceRef} className="custom-range w-100" defaultValue="0" min="0" max="10" id={"restaurant" + c.pk} onClick={(e)=>  this.handleResistanceCreate(e,c.pk) }></input>
+                        <input type="range" ref={this.resistanceRef} className="custom-range w-100" defaultValue="0" min="0" max="10" id={"restaurant" + c.pk} onClick={(e)=> this.handleSliderChange(e, c.pk)}></input>
                         <div className="d-flex justify-content-between">
                           <span>Nope</span>
                           <span>Yeah</span>
                         </div>
                     </div>)}
-                    <input className="btn btn-primary" type="submit" value="Submit" />
-                  </form>
+                    <button className="btn btn-primary" type="submit" value="Submit" onClick={this.handleFormSubmit}>Submit</button>
                 </div>
             </div>
         );
