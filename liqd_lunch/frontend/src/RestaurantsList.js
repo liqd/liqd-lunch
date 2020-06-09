6c375174c58a66de1019303b2926cb55e4cae1cb
Robert Mader <robert.mader@posteo.de>
2020-06-09 17:33:18 +0200

    frontend/RestaurantsList: Use react redirect (and some cleanups)
    As the routes we are using are defined in react, we have to access them
    accordingly to make sure they work reliably.
    
    See https://dev.to/projectescape/programmatic-navigation-in-react-3p1l

--- liqd_lunch/frontend/src/RestaurantsList.js
+++ liqd_lunch/frontend/src/RestaurantsList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 import ResistanceService from './ResistanceService';
 import RestaurantsService from './RestaurantsService';
 
@@ -12,7 +13,8 @@ class RestaurantsList extends Component {
     this.state  = {
       restaurants: [],
       nextPageURL: '',
-      resistanceScore: {}
+      resistanceScore: {},
+      redirect: null
     };
     this.nextPage = this.nextPage.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
@@ -24,7 +26,7 @@ class RestaurantsList extends Component {
     const self = this
     if (self.timeForLunch()) {
       if (window.confirm('Voting is closed, it is LUNCH TIME! See the result?')) {
-        window.location.href = "/resistances";
+        this.setState({ redirect: "/resistances" });
       }
     }
     restaurantsService.getRestaurants().then(function (result) {
@@ -86,30 +88,34 @@ class RestaurantsList extends Component {
   }
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }
 
-        return (
-            <div className="restaurants--list row justify-content-center">
-              <div className="col-sm-8 col-md-6 col-lg-4">
-                {this.state.restaurants.map( c  =>
-                    <div className="p-3 form-group" key={c.pk}>
-                        <div className="d-flex justify-content-between">
-                            <label htmlFor={"restaurant" + c.pk}>{c.name}</label>
-                            <span>
-                              <a className="btn btn-link btn-sm text-muted" href={"/restaurant/" + c.pk}> Update</a>
-                              <button className="btn btn-link btn-sm text-muted" onClick={(e)=>  this.handleDelete(e, c.pk) }> Delete</button>
-                            </span>
-                        </div>
-                        <input type="range" ref={this.resistanceRef} className="custom-range mt-2 w-100" defaultValue="0" min="0" max="10" id={"restaurant" + c.pk} onClick={(e)=> this.handleSliderChange(e, c.pk)}></input>
-                        <div className="d-flex justify-content-between">
-                          <span>Yeah</span>
-                          <span>Nope</span>
-                        </div>
-                    </div>)}
-                    <div className="d-flex justify-content-end px-3">
-                      <button className="btn btn-dark text-uppercase ml-auto" type="submit" value="Submit" onClick={this.handleFormSubmit}>Submit</button>
-                    </div>
-                </div>
+    return (
+      <div className="restaurants--list row justify-content-center">
+        <div className="col-sm-8 col-md-6 col-lg-4">
+          {this.state.restaurants.map( c  =>
+            <div className="p-3 form-group" key={c.pk}>
+              <div className="d-flex justify-content-between">
+                <label htmlFor={"restaurant" + c.pk}>{c.name}</label>
+                <span>
+                  <a className="btn btn-link btn-sm text-muted" href={"/restaurant/" + c.pk}> Update</a>
+                  <button className="btn btn-link btn-sm text-muted" onClick={(e)=>  this.handleDelete(e, c.pk) }> Delete</button>
+                </span>
+              </div>
+              <input type="range" ref={this.resistanceRef} className="custom-range mt-2 w-100" defaultValue="0" min="0" max="10" id={"restaurant" + c.pk} onClick={(e)=> this.handleSliderChange(e, c.pk)}></input>
+              <div className="d-flex justify-content-between">
+                <span>Yeah</span>
+                <span>Nope</span>
+              </div>
             </div>
+          )}
+          <div className="d-flex justify-content-end px-3">
+            <button className="btn btn-dark text-uppercase ml-auto" type="submit" value="Submit" onClick={this.handleFormSubmit}>Submit</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
